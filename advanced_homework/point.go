a431a904ba60356517e841cf97e8b7c09336d1c3
KEVIN711-1 <928085237@qq.com>
2025-11-30 22:04:16 +0800

    [go_learning] point_num_add and point_num_arrary

--- advanced_homework/point.go
+++ advanced_homework/point.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func point_1(target *int) {
-	*target += 10
+func point_num_add(target *int, add_num int) {
+	*target += add_num
 }
 
-func point_2(target []int) {
+func point_array_multi(target []int, multi_num int) {
 	for index, value := range target {
-		target[index] = value * 2
+		target[index] = value * multi_num
 	}
 }
 
@@ -17,13 +17,13 @@ func main() {
 	num := 10
 	fmt.Println("before point_1:", num)
 
-	point_1(&num)
+	point_num_add(&num, 10)
 	fmt.Println("after point_1:", num)
 
 	num_array := []int{10, 1, 2, 3, 4, 5, 70}
 	fmt.Println("before point_2:", num_array)
 
-	point_2(num_array)
+	point_array_multi(num_array, 2)
 	fmt.Println("after point_2:", num_array)
 
 }
